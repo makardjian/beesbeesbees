8a2bf86c65b619dcd97886883eeabeae807f4349
Michael Kardjian <makardjian@gmail.com>
2018-11-12 23:18:36 -0800

    completed all the subclasses in pseudoclassical style. All tests are passing.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+	Bee.call(this);
+	this.age = 10,
+	this.job = 'make honey',
+	// this.color is inhereted from Bee
+	//food property is inhereted from Grub
+	this.honeyPot = 0
+
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+	this.honeyPot++
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+	this.honeyPot--
+}
 
